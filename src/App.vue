23018a17a3824fbdd8bfd8efee4ed09158b1b0fb
Ziedelth <watteaupierre@gmail.com>
2022-03-04 22:19:47 +0100

    Async/Await

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,64 @@
 <template>
-  <div class="bg-full-dark text-white">
-    <Navbar/>
-
-    <main class="my-3 vertical-center text-center w-100">
+  <div class="bg-full-dark text-white" style="min-height: 100vh">
+    <div v-if="isLoading" class="vertical-center text-center w-100">
       <div class="container-fluid">
-        <router-view :key="$route.fullPath"/>
+        <LoadingComponent is-loading />
       </div>
-    </main>
+    </div>
 
-    <Footer/>
+    <div v-else>
+      <Navbar/>
+
+      <main class="my-3 vertical-center text-center w-100">
+        <div class="container-fluid">
+          <router-view :key="$route.fullPath"/>
+        </div>
+      </main>
+
+      <Footer/>
+    </div>
   </div>
 </template>
 
 <script>
 import Utils from "@/utils";
 import {mapGetters, mapState} from "vuex";
+import LoadingComponent from "@/components/LoadingComponent";
 
 const Navbar = () => import("@/components/NavbarComponent");
 const Footer = () => import("@/components/FooterComponent");
 
 export default {
-  components: {Footer, Navbar},
+  components: {LoadingComponent, Footer, Navbar},
   computed: {
     ...mapState(['user'])
   },
   methods: {
     ...mapGetters(['isLogin'])
   },
+  data() {
+    return {
+      isLoading: true,
+    }
+  },
   async mounted() {
-    await Utils.get(`api/v1/countries`, 200, (success) => {
-      console.log(success[0])
+    this.isLoading = true
 
+    await Utils.get(`api/v1/countries`, 200, (success) => {
       this.$store.dispatch('setCountries', success)
       this.$store.dispatch('setCurrentCountry', success[0])
     }, (failed) => null)
 
+    this.isLoading = false
     this.$session.start()
 
-    if (this.isLogin())
-      return
-
-    if (!this.$session.has('token'))
+    if (this.isLogin() || !this.$session.has('token'))
       return
 
     await Utils.post(`php/v1/member/get_user.php`, JSON.stringify({token: this.$session.get('token')}), 200, (success) => {
       this.$store.dispatch('setToken', this.$session.get('token'))
       this.$store.dispatch('setUser', success)
-    }, (failed) => {
-    })
+    }, (failed) => null)
   }
 }
 </script>
