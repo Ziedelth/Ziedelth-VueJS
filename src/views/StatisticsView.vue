449a6aac2eedff77772f213ed1a48abe65a46670
Ziedelth <watteaupierre@gmail.com>
2022-03-30 22:47:09 +0200

    Add timer

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -19,46 +19,57 @@ export default {
   data() {
     return {
       isLoading: true,
+      interval: null,
       globalDataMonth: null,
     }
   },
+  methods: {
+    async fetchData() {
+      await Utils.get(`api/v1/statistics/30`, (success) => {
+        const globalDataset = [
+          {
+            label: "Total",
+            borderColor: "#ffffff",
+            borderWidth: 2,
+            data: success.map((item) => item.episodes + item.scans),
+          },
+        ];
+
+        success.forEach(data => {
+          data.platforms.forEach(item => {
+            if (globalDataset.some(data => data.label === item.name)) {
+              globalDataset.filter(data => data.label === item.name).forEach(data => {
+                data.data.push(item.episodes + item.scans);
+              });
+            } else {
+              globalDataset.push({
+                label: item.name,
+                borderColor: "#" + item.color.toString(16).padStart(6, "0"),
+                borderWidth: 2,
+                data: [item.episodes + item.scans],
+              });
+            }
+          });
+        });
+
+        this.globalDataMonth = {
+          labels: success.map((item) => item.date),
+          datasets: globalDataset
+        };
+      }, (failed) => null)
+    },
+  },
   async mounted() {
     this.isLoading = true;
-
-    await Utils.get(`api/v1/statistics/30`, (success) => {
-      const globalDataset = [
-        {
-          label: "Total",
-          borderColor: "#ffffff",
-          borderWidth: 2,
-          data: success.map((item) => item.episodes + item.scans),
-        },
-      ];
-
-      success.forEach(data => {
-        data.platforms.forEach(item => {
-          if (globalDataset.some(data => data.label === item.name)) {
-            globalDataset.filter(data => data.label === item.name).forEach(data => {
-              data.data.push(item.episodes + item.scans);
-            });
-          } else {
-            globalDataset.push({
-              label: item.name,
-              borderColor: "#" + item.color.toString(16).padStart(6, "0"),
-              borderWidth: 2,
-              data: [item.episodes + item.scans],
-            });
-          }
-        });
-      });
-
-      this.globalDataMonth = {
-        labels: success.map((item) => item.date),
-        datasets: globalDataset
-      };
-    }, (failed) => null)
-
+    await this.fetchData();
     this.isLoading = false;
-  }
+
+    this.interval = setInterval(() => {
+      this.fetchData();
+    }, 60 * 1000)
+  },
+  destroyed() {
+    clearInterval(this.interval);
+  },
 }
 </script>
